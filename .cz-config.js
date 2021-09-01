bd72d35c99ea4dcdd61648f666a9dd6e6798da09
XuZehong <xuzehong@sat-tech.cn>
2021-09-01 10:48:40 +0800

    docs(.cz-config.js): init

--- .cz-config.js
+++ .cz-config.js
@@ -3,7 +3,7 @@
 module.exports = {
     types: [
         { value: 'feat', name: 'feat:     新功能' },
-        { value: 'fix', name: 'fix:      修复' },
+        { value: 'fix', name: 'fix:      修复bug' },
         { value: 'docs', name: 'docs:     文档变更' },
         { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
         { value: 'refactor', name: 'refactor: 重构(既不是增加feature，也不是修复bug)' },
