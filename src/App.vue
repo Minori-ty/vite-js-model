a309600940b0565adeb5541b9474cbec66c4c810
守夜人 <1229248506@qq.com>
2021-09-15 21:47:15 +0800

    fix(styles): native

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div>1</div>
-    <img src="/images/1.png" alt="" />
-    <img src="./assets/images/1.png" alt="" />
+    <img src="images/1.png" alt="" />
+    <h1>1</h1>
+    <!-- <img src="./assets/images/1.png" alt="" /> -->
     <img src="./assets/images/pic/contact.jpg" alt="" />
 </template>
 
@@ -10,9 +11,7 @@ import { isMobile } from 'utils/isMobile.js'
 let flag1 = isMobile() ?? 'null'
 window.onresize = () => {
     let flag2 = isMobile() ?? 'null'
-    if (flag1[0] != flag2[0]) {
-        location.reload()
-    }
+    if (flag1[0] != flag2[0]) location.reload()
 }
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,28 @@ import styleImport from 'vite-plugin-style-import'
 import viteCompression from 'vite-plugin-compression'
 import externalGlobals from 'rollup-plugin-external-globals'
 import ViteComponents, { ElementPlusResolver } from 'vite-plugin-components'
-// import viteImagemin from 'vite-plugin-imagemin'
+import viteImagemin from 'vite-plugin-imagemin'
 import html from 'vite-plugin-html'
 
 export default defineConfig({
     base: './',
+    resolve: {
+        alias: {
+            '/images': 'src/assets/images',
+            images: resolve(__dirname, 'src/assets/images'),
+            styles: resolve(__dirname, 'src/styles'),
+            router: resolve(__dirname, 'src/router'),
+            views: resolve(__dirname, 'src/views'),
+            components: resolve(__dirname, 'src/components'),
+            data: resolve(__dirname, 'src/data'),
+            utils: resolve(__dirname, 'src/utils'),
+            src: resolve(__dirname, 'src'),
+            assets: resolve(__dirname, 'src/assets'),
+            mobile: resolve(__dirname, 'src/views/Mobile'),
+            PC: resolve(__dirname, 'src/views/PC'),
+            '/pic': 'src/assets/images/pic',
+        },
+    },
     plugins: [
         vue(),
         // 按需引入组件
@@ -32,54 +49,38 @@ export default defineConfig({
             algorithm: 'gzip',
             ext: '.gz',
         }),
-        // viteImagemin({
-        //     gifsicle: {
-        //         optimizationLevel: 7,
-        //         interlaced: false,
-        //     },
-        //     optipng: {
-        //         optimizationLevel: 7,
-        //     },
-        //     mozjpeg: {
-        //         quality: 50,
-        //     },
-        //     pngquant: {
-        //         quality: [0.8, 0.9],
-        //         speed: 4,
-        //     },
-        //     svgo: {
-        //         plugins: [
-        //             {
-        //                 name: 'removeViewBox',
-        //             },
-        //             {
-        //                 name: 'removeEmptyAttrs',
-        //                 active: false,
-        //             },
-        //         ],
-        //     },
-        // }),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 50,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+        }),
         html({
             minify: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '/images': 'src/assets/images',
-            images: 'src/assets/images',
-            styles: resolve(__dirname, 'src/styles'),
-            router: resolve(__dirname, 'src/router'),
-            views: resolve(__dirname, 'src/views'),
-            components: resolve(__dirname, 'src/components'),
-            data: resolve(__dirname, 'src/data'),
-            utils: resolve(__dirname, 'src/utils'),
-            src: resolve(__dirname, 'src'),
-            assets: resolve(__dirname, 'src/assets'),
-            mobile: resolve(__dirname, 'src/views/Mobile'),
-            PC: resolve(__dirname, 'src/views/PC'),
-            '/pic': 'src/assets/images/pic',
-        },
-    },
+
     // 引入全局scss文件
     css: {
         preprocessorOptions: {
@@ -99,23 +100,23 @@ export default defineConfig({
         // 取消计算文件大小，加快打包速度
         brotliSize: false,
         sourcemap: true,
-        assetsDir: 'static/img/',
-        rollupOptions: {
-            output: {
-                chunkFileNames: 'static/js/[name]-[hash].js',
-                entryFileNames: 'static/js/[name]-[hash].js',
-                assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-            },
+        // assetsDir: 'static/img',
+        // rollupOptions: {
+        //     output: {
+        //         chunkFileNames: 'static/js/[name]-[hash].js',
+        //         entryFileNames: 'static/js/[name]-[hash].js',
+        //         // assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        //     },
 
-            // 配置CDN
-            external: ['element-plus', 'vue'],
-            plugins: [
-                externalGlobals({
-                    vue: 'Vue',
-                    'element-plus': 'ElementPlus',
-                }),
-            ],
-        },
+        //     // 配置CDN
+        //     external: ['element-plus', 'vue'],
+        //     plugins: [
+        //         externalGlobals({
+        //             vue: 'Vue',
+        //             'element-plus': 'ElementPlus',
+        //         }),
+        //     ],
+        // },
     },
     server: {
         host: '0.0.0.0',
