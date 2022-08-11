5098c1f6b947d9b728cd3eec0e132d1ed4a9671b
Xu Zehong <xuzehong@sat-tech.cn>
2022-08-11 14:03:56 +0800

    test

--- .cz-config.js
+++ .cz-config.js
@@ -2,16 +2,16 @@
 
 module.exports = {
     types: [
-        { value: 'feat', name: 'feat:     新功能' },
-        { value: 'fix', name: 'fix:      修复bug' },
-        { value: 'docs', name: 'docs:     文档变更' },
-        { value: 'style', name: 'style:    代码格式(不影响代码运行的变动)' },
-        { value: 'refactor', name: 'refactor: 重构(既不是增加feature，也不是修复bug)' },
-        { value: 'perf', name: 'perf:     性能优化' },
-        { value: 'test', name: 'test:     增加测试' },
-        { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
+        { value: ':sparkles:', name: 'feat:     新功能' },
+        { value: ':wrensh:', name: 'fix:      修复bug' },
+        { value: ':page_facing_up:', name: 'docs:     文档变更' },
+        { value: ':art:', name: 'style:    代码格式(不影响代码运行的变动)' },
+        { value: ':recycle:', name: 'refactor: 重构(既不是增加feature，也不是修复bug)' },
+        { value: ':zap:', name: 'perf:     性能优化' },
+        { value: ':white_check_mark:', name: 'test:     增加测试' },
+        { value: ':memo:', name: 'chore:    构建过程或辅助工具的变动' },
         { value: 'revert', name: 'revert:   代码回退' },
-        { value: 'build', name: 'build:    变更项目构建或外部依赖' },
+        { value: ':tada:', name: 'build:    变更项目构建或外部依赖' },
     ],
     scopes: [
         ['api', 'api相关'],
