e468ec2f36e57fb0c4e7faf961ede63f0ea7709c
Xu Zehong <xuzehong@sat-tech.cn>
2022-08-11 15:56:25 +0800

    sparkles(hooks): 添加缓存

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     'src/**/*.{js,ts,vue}': [
-        'prettier --cache --write "src/**/*.{ts,tsx,vue}"',
+        'prettier --cache --write "src/**/*.{ts,tsx,vue,less,scss,css}"',
         'eslint --cache "src/**/*.{ts,tsx,vue}"',
     ],
 }
